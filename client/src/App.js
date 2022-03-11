f74736d581f7eaf105c9567268db02035c5a1c91
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-03-11 16:31:48 -0500

    Setup basic frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from "socket.io-client";
+import {v4 as uuidV4} from 'uuid';
+import React from "react";
 
 function App() {
+  const [socketHandler, setSocketHandler] = React.useState(undefined);
+  const [message, setMessage] = React.useState("");
+
+  const [chatMessages, setChatMessages] = React.useState([
+    {
+      id: 'someId1',
+      user: {
+        name: 'Bob',
+        // socketId: 'someSocketId'
+      },
+      message: 'sampleMessage'
+    }
+  ]);
+
+  const [user, setUser] = React.useState(undefined);
+
+  React.useEffect(() => {
+    const socket = socketIOClient('http://localhost:8081', {secure: false});
+
+    setSocketHandler(socket);
+
+    setUser({
+      name: "Pranav",
+      // socketId: socket.id
+    });
+
+    console.log(socket);
+
+    return () => {
+      if (socketHandler) {
+        socketHandler.close();
+      }
+    }
+  }, []);
+
+  // function to send a message
+  const sendMessage = (message) => {
+    // prevent empty message from being sent
+    if (message || message.trim().length > 0) {
+      if (socketHandler) {
+        const messageObj = {
+          id: uuidV4(),
+          user: user,
+          message: message
+        };
+
+        console.log('sending message object', messageObj);
+
+        // emit message to socket server
+        socketHandler.emit('chat-message', messageObj);
+
+        // clear text field after sending message
+        setMessage('');
+      } else {
+        console.warn('socket handler not defined, unable to send message');
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {
+          chatMessages.map((message) => {
+            return (
+                <div key={message.id} style={{backgroundColor: "grey", margin: "10px"}}>
+                  <p style={{color: "white"}}>{message.user.name}</p>
+                  <p style={{color: "white"}}>{message.message}</p>
+                </div>
+            );
+          })
+        }
+      </div>
+      <input value={message} onChange={(event) => {
+        console.log(event.target.value);
+        setMessage(event.target.value);
+      }}/>
+      <button onClick={() => {sendMessage(message)}}>Send Message</button>
     </div>
   );
 }
